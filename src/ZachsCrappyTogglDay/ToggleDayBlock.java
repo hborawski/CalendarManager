181d4b1e09fb274ef66c732d9c491df4df79cd6f
Harris Borawski <harrisborawski@me.com>
2013-04-30 15:08:43 -0400

    Metrics can run now

--- src/ZachsCrappyTogglDay/ToggleDayBlock.java
+++ src/ZachsCrappyTogglDay/ToggleDayBlock.java
@@ -8,6 +8,8 @@ import java.awt.event.MouseListener;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Set;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,6 +17,9 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JToggleButton;
 import javax.swing.border.EtchedBorder;
+
+import com.std.model.CalendarModelUtility;
+import com.std.model.appointment.RefAppointment;
 
 public class ToggleDayBlock extends JToggleButton {
 
@@ -69,10 +74,10 @@ public class ToggleDayBlock extends JToggleButton {
 				temp.setBackground(new Color(0x00FFFFFF, true));
 				temp.setOpaque(false);
 			}
-			for (RefAppointment added : addedAppointments) {
-				if (CalendarModelUtiltiy.conflictingAppointments(added, app)) {
+			for (RefAppointment added : addedAppointments.keySet()) {
+				if (CalendarModelUtility.conflictingAppointments(added, app)) {
 
-					addedAppointments.get(added).addApointment(temp);
+					addedAppointments.get(added).addAppointment(temp);
 					addedAppointments.put(app, addedAppointments.get(added));
 					hasAdded = true;
 				}
@@ -98,10 +103,10 @@ public class ToggleDayBlock extends JToggleButton {
 	public void addAppointmentListener(MouseListener aL) {
 		apptListener = aL;
 	}
-	
+	/*
 	public static void main(String[] args) {
 		JFrame f = new JFrame();
-		ToggleDayBlock t = new ToggleDayBlock();
+		ToggleDayBlock t = new ToggleDayBlock(new Date());
 		t.update(Calendar.getInstance().getTime());
 		f.add(t);
 		f.setVisible(true);
@@ -109,5 +114,6 @@ public class ToggleDayBlock extends JToggleButton {
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
+	*/
 
 }
